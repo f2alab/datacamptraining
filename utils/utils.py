119d49928583e0493721b7b4533c8786100c3497
GEEK <agaffabien@gmail.com>
2025-05-15 17:23:51 +0000

    update3

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,9 @@
 
+
 # Draw a line of stars with a specified character and margin
+from pandas import DataFrame
+
+
 def draw_stars_line(n=50, char='*', margin=2):
     """
     Draw a line of stars with a specified character and margin.
@@ -13,3 +17,16 @@ def draw_stars_line(n=50, char='*', margin=2):
     padding  = "\n" * margin
 
     print(f"{padding}{char * n}", end= padding if margin > 0 else '')
+
+
+# Searc specific string in columns of a DataFrame
+def search_string_in_columns(df : DataFrame, search_string: str) -> list:
+    """
+    Search for a specific string in all columns of a DataFrame.
+    Parameters:
+    df (pd.DataFrame): The DataFrame to search in.
+    search_string (str): The string to search for.
+    Returns:
+    list: A list of columns contains search_string.
+    """
+    return [col for col in df.columns if search_string in col]
